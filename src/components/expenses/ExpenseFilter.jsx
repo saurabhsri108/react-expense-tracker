a50b5cb97bbeb39daaaf78ad409643334df941ad
Saurabh Srivastava <vasudeveloper001@gmail.com>
2021-06-15 14:08:33 +0530

    Working on Expense Filter

--- src/components/expenses/ExpenseFilter.jsx
+++ src/components/expenses/ExpenseFilter.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const ExpenseFilter = () => {
+const ExpenseFilter = ({ filterExpense }) => {
   return (
     <section className='expense-filter-container'>
       <label htmlFor='filter-date'>Filter</label>
-      <select name='filter-date' id='filter-date'>
+      <select
+        name='filter-date'
+        id='filter-date'
+        onChange={(e) => filterExpense(e.target.value)}
+      >
         <option value='2020'>2020</option>
         <option value='2021'>2021</option>
         <option value='2022'>2022</option>
--- src/components/expenses/ExpenseLists.jsx
+++ src/components/expenses/ExpenseLists.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../utitlity/Card';
 import Expense from './Expense';
 import ExpenseFilter from './ExpenseFilter';
 
 const ExpenseLists = ({ expensesList }) => {
-  console.log(expensesList);
+  const [filterExpenses, setFilterExpenses] = useState(expensesList);
+  console.log(filterExpenses);
+  const filterExpenseHandler = (dateValue) => {
+    setFilterExpenses((previousFilterExpenses) => {
+      const afterFilterExpenses = previousFilterExpenses.filter((expense) => {
+        expense.date.getFullYear().toString() === dateValue;
+      });
+      console.log(afterFilterExpenses);
+      return afterFilterExpenses;
+    });
+  };
+
   return (
     <Card className='expense-list'>
       <h2 className='text-center'>Expense Lists</h2>
-      <ExpenseFilter />
-      {expensesList.map((expense) => (
+      <ExpenseFilter filterExpense={filterExpenseHandler} />
+      {filterExpenses.map((expense) => (
         <Expense key={expense.id} {...expense} />
       ))}
     </Card>
